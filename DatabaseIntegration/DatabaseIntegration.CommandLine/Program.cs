473f7c2a9a1e52239fee10178c26f65bf83c5899
Chris Harrison <cah@stpaulsschool.org.uk>
2021-05-10 11:10:37 +0100

    Seeded with the basic VS solution

---
+++ DatabaseIntegration/DatabaseIntegration.CommandLine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using DatabaseIntegration.Lib;
+
+namespace DatabaseIntegration.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionStringSettings cxnstring = ConfigurationManager.ConnectionStrings["sl"];
+
+            MovieDatabase md = new MovieDatabase(cxnstring.ConnectionString);
+
+            foreach (string s in md.MoviesByRating(5))
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DatabaseIntegration/DatabaseIntegration.Lib/MovieDatabase.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+
+namespace DatabaseIntegration.Lib
+{
+    public class MovieDatabase
+    {
+        // interop error https://stackoverflow.com/a/41700451/2902
+
+        readonly string _connectionString;
+        public const string N_STAR_MOVIES = "select distinct title, director from movies m join ratings r on m.id = r.mid where r.rating = {0} ";
+
+        public MovieDatabase(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<string> MoviesByRating(int r)
+        {
+            string qry = string.Format(N_STAR_MOVIES, r);
+            return GetMeAList(qry);
+        }
+
+
+        public IEnumerable<string> GetMeAList(string qry)
+        {
+            using (SQLiteConnection cxn = new SQLiteConnection(_connectionString))
+            {
+                cxn.Open();
+                SQLiteCommand cmd;
+                cmd = cxn.CreateCommand();
+                cmd.CommandText = qry;
+
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    // return the second column 
+                    yield return reader.GetString(1);
+                }
+            }
+        }
+
+        public DataSet GetMeSomeData(string qry)
+        {
+            DataSet ds = null;
+            // use 'using' the avoid explicitly closing the connection 
+            using (SQLiteConnection cxn = new SQLiteConnection(_connectionString))
+            {
+                // create a command object based on a query and a connection 
+                SQLiteCommand cmd = new SQLiteCommand(qry, cxn);
+                cmd.CommandType = CommandType.Text;
+                // pass the command to an adapter 
+                SQLiteDataAdapter ad = new SQLiteDataAdapter(cmd);
+                // use the adapter to fill a dataset 
+                ds = new DataSet();
+                // if the location is unwritable, you'll get an error here 
+                ad.Fill(ds);
+            }
+
+            return ds;
+        }
+    }
+}
